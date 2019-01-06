ad71524d0023c2458b8e5592968e00d68bcf6dea
Douglas Miller <durgadas311@gmail.com>
2019-01-06 17:21:12 -0600

    Fixes for building INITDIR.COM

--- dist/cmd/src/mcd80d.asm
+++ dist/cmd/src/mcd80d.asm
@@ -1,90 +1,39 @@
-$title	('COM Externals')
-	name	mcd80a
+;	title	mcd80d
 	CSEG
 ;	September 14, 1982
 
-offset	equ	0000h
-boot	equ	0000h	;[JCE] to make SHOW compile
-
-	EXTRN	PLM
-
-;	EXTERNAL ENTRY POINTS
-
-mon1	equ	0005h+offset
-mon2	equ	0005h+offset
-mon2a	equ	0005h+offset
-mon3 	equ	0005h+offset
-	public	mon1,mon2,mon2a,mon3
-
-;	EXTERNAL BASE PAGE DATA LOCATIONS
-
-iobyte	equ	0003h+offset
-bdisk	equ	0004h+offset
-maxb	equ	0006h+offset
-memsiz	equ	maxb
-cmdrv	equ	0050h+offset
-pass0	equ	0051h+offset
-len0	equ	0053h+offset
-pass1	equ	0054h+offset
-len1	equ	0056h+offset
-fcb	equ	005ch+offset
-fcba	equ	fcb
-sfcb	equ	fcb
-ifcb	equ	fcb
-ifcba	equ	fcb
-fcb16	equ	006ch+offset
-dolla	equ	006dh+offset
-parma	equ	006eh+offset
-cr	equ	007ch+offset
-rr	equ	007dh+offset
-rreca	equ	rr
-ro	equ	007fh+offset
-rreco	equ	ro
-tbuff	equ	0080h+offset
-buff	equ	tbuff
-buffa	equ	tbuff
-cpu	equ	0	; 0 = 8080, 1 = 8086/88, 2 = 68000
-
-	public	iobyte,bdisk,maxb,memsiz
-	public	cmdrv,pass0,len0,pass1,len1
-	public	fcb,fcba,sfcb,ifcb,ifcba,fcb16
-	public	cr,rr,rreca,ro,rreco,dolla,parma
-	public	buff,tbuff,buffa, cpu, boot
-
-
-	;*******************************************************
-	; The interface should proceed the program
-	; so that TRINT becomes the entry point for the 
-	; COM file.  The stack is set and memsiz is set
-	; to the top of memory.  Program termination is done
-	; with a return to preserve R/O diskettes.
-	;*******************************************************
+	EXTRN	INITDIR
 
 ;	EXECUTION BEGINS HERE
-
 ;
-;[JCE 17-5-1998] Guard code prevents this program being run under DOS
-;
-	db	0EBh,7		;Sends 8086s to I8086:
-	lxi	sp, stack
-	JMP 	PLM
-	db	0		;Packing.
-;
-I8086:	db	0CDh,020h	;INT 20h - terminate immediately
+;	JMP 	around	; JMP inserted by LINK
 
 ;	PATCH AREA, DATE, VERSION & SERIAL NOS.
 
+patch1:	shld 777ah
+	shld 7a7ch
+	jmp 0d01h
+
+patch2:	lxi h,4661h
+	push h
+	mvi m,'c'
+	inx h
+	mvi m,'F'
+	pop h
+	call 1003h
+
+;	dw	0,0,0,0,0,0,0,0
+	dw	0,0,0,0,0; 0,0,0
 	dw	0,0,0,0,0,0,0,0
-	dw	0,0,0,0,0,0,0,0
-	dw	0,0,0,0,0,0,0,0
-	dw	0,0
+	dw	0,0,0,0,0,0
 	db	0
 	db	'CP/M Version 3.0'
-	db	'Copyright 1998, '
-	db	'Caldera, Inc.   '
-	db	'190598'	; version date day-month-year
-	db	0,0,0,0		; patch bit map
+	db	'COPYRIGHT 1982, '
+	db	'DIGITAL RESEARCH'
+	db	'151282'	; version date day-month-year
+	db	0,0,0,3		; patch bit map
 	db	'654321'	; Serial no.
+around:
+	db	0,0,0
 
 	END
-	EOF
