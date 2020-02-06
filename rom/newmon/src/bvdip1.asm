0f69e34f5f14d329bdf907f544786a7233f3b1fe
Douglas Miller <durgadas311@gmail.com>
2020-02-06 17:36:43 -0600

    Working VDIP1 boot

--- rom/newmon/src/bvdip1.asm
+++ rom/newmon/src/bvdip1.asm
@@ -8,8 +8,9 @@ vdip1	equ	0d8h	; assume part of Z80-DUART
 	maclib	z80
 
 CR	equ	13
-vdbuf	equ	2280h
-vdscr	equ	2300h
+bbuf:	equ	2280h
+vdbuf	equ	2300h
+vdscr	equ	2380h
 
 	org	1000h
 first:	db	HIGH (last-first)	; +0: num pages
@@ -34,9 +35,20 @@ boot:
 	lxi	h,opr
 	lxi	d,vdbuf
 	call	strcpy
-	lxi	h,defbt
+	push	d	; current end of OPR command string
+	lxi	h,bbuf	; possible string here
+	mov	a,m
+	cpi	0c3h	; JMP means no string
+	jrz	boot5
+	; else, A=str len
+	mov	c,a
+	mvi	b,0
+	inx	h
+	ldir	; DE points past end of name
+	jr	boot6
+boot5:	lxi	h,defbt
 	call	strcpy
-	mvi	a,CR
+boot6:	mvi	a,CR
 	stax	d
 	lxi	h,vdbuf
 	call	vdcmd	; open file
@@ -45,21 +57,92 @@ boot:
 	call	vdrd
 	jrc	bootx
 	; TODO: get load parameters..
+	lhld	vdscr
+	shld	memtop
+	lhld	vdscr+2
+	shld	bnktop
+	lhld	vdscr+4
+	shld	entry
+	lda	vdscr+16	; ORG0 flag - don't care?
+	sta	copy
 	lxi	h,vdscr
 	call	vdrd
 	jrc	bootx
-	lxi	h,vdscr	; load message
-	call	msgout	; TODO: strip '$'
-	;
+	lxi	d,vdscr	; load message
+	call	print
+	lda	comlen
+	ora	a
+	jrz	boot1
+	mov	d,a
+	lda	memtop
+	call	loadit
+	rc
+boot1:	lda	bnklen
+	ora	a
+	jrz	boot2
+	mov	d,a
+	lda	bnktop
+	call	loadit
+	rc
+boot2:	; ready to go?
+	call	bootx	; close file
+	; cleanup clocks...
+	di
+	mvi	a,10011111b	; H8 2mS off, display blank
+	out	0f0h
+	; already at ORG0... H89 2mS already off?
+	lhld	entry
+	pchl
+
 bootx:	; exit boot on error, must close file
 	lxi	h,clf
 	call	vdcmd
+	ret
+
+; A=top page (might be 0 for 64K)
+; D=num pages
+loadit:	mov	h,a
+	mvi	l,0	; HL=top address
+	ora	a
+	ralr	d	; num records
+load0:
+	lxi	b,-128
+	dad	b
+	push	h
+	push	d
+	call	vdrd
+	pop	d
+	pop	h
+	rc		; error
+	dcr	d
+	jrnz	load0
+	ora	a	; NC
 	ret
 
 rdf:	db	'rdf ',0,0,0,128,CR
 clf:	db	'clf',CR
 defbt:	db	'defboot.sys',0	; default boot file
 opr:	db	'opr ',0	; command segment
+
+memtop:	db	0
+comlen:	db	0
+bnktop:	db	0
+bnklen:	db	0
+entry:	dw	0
+; don't care about cfgtbl?
+copy:	db	0	; 'C' if ORG0 required
+
+; BDOS-style print function
+; DE=message, '$' terminated
+print:	ldax	d
+	cpi	'$'
+	rz
+	call	outcon
+	inx	d
+	jr	print
+
+outcon:	lhld	conout
+	pchl
 
 	maclib	vdip1
 
