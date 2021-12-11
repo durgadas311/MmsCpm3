a2b76ee7e61a09e5f6425301ef8128f1d3244553
Douglas Miller <durgadas311@gmail.com>
2021-12-11 13:09:12 -0600

    Add Software Toolworks MATHPAK

---
+++ 3p/swtw-c80/math/fprintf.h
@@ -0,0 +1,24 @@
+/* fprintf.h: definitions for printf and fprintf to allow multiple args.
+ */
+
+#undef printf
+#undef fprintf
+#undef sprintf
+#define printf prnf_1(),prnf_2
+#define fprintf prnf_1(),prnf_3
+#define sprintf prnf_1(),prnf_4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
