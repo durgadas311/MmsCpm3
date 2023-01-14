99071944fe8d8b04ca05efc9aa7f6c97d0684432
Douglas Miller <durgadas311@gmail.com>
2023-01-13 18:17:21 -0600

    rom2-80: get basic memory test working

--- rom/newmon-80/src/cmemtst.asm
+++ rom/newmon-80/src/cmemtst.asm
@@ -11,6 +11,10 @@ CR	equ	13
 LF	equ	10
 BEL	equ	7
 
+; ctl$F2 bits
+ctl$SPD		equ	00010100b
+ctl$ORG0	equ	00100000b
+
 	org	1000h
 first:	db	HIGH (last-first)
 	db	HIGH first
@@ -29,12 +33,9 @@ init:	xra	a
 	ret
 
 mtms:	db	'em test',0
-cserms:	db	BEL,'Cksum error',0
-topms:	db	'Top of Mem: ',0
+topms:	db	'Testing 2000-FFFF',CR,LF,0
 
-cserr:
-	lxi	h,cserms
-	jmp	msgout
+seed:	db	0
 
 cmdmt:
 	lxi	h,mtms
@@ -42,99 +43,20 @@ cmdmt:
 	call	waitcr
 	lxi	h,topms
 	call	msgout
-	lxi	h,0
-	dad	sp
-	mov	a,h
-	inr	a
-	jz	cmdmt0
-	sui	020h
-cmdmt0:
-	mov	h,a
-	mvi	l,0
-	dcx	h
-	sui	'0'
-	mov	e,a
-	call	adrout
-	call	crlf
-if 1 ; TODO: de-zilog memory test
-	.warning	'de-zilog memory test'
-	ret
-else ; TODO: de-zilog memory test
-	mvi	d,000h
-	mvi	c,030h
-	mvi	b,000h
-	exx
-	lxi	h,mtest0
-	lxi	d,memtest - (mtest1-mtest0)
-	lxi	b,mtestZ-mtest0
-	call	ldir
-	lxi	d,memtest
-	lxi	h,mtest1
-	mvi	c,mtestZ-mtest1
-	xra	a
-	exaf
-	xra	a
-cmdmt1:
-	add	m
-	exaf
-	xchg
-	add	m
-	exaf
-	xchg
-	inx	h
-	inx	d
-	dcr	c
-	jnz	cmdmt1
-	mov	c,a
-	exaf
-	cmp	c
-	jnz	cserr
 	di
+	lxi	sp,stack
 	lda	ctl$F2
 	ani	ctl$SPD	; all but speed bits OFF
-	ori	ctl$ORG0	; set ORG0 only
-	; pass ctl$F2 in A...
-	jmp	memtest - (mtest1-mtest)
-
-;------------------------------------------------
-; Start of relocated code...
-; Memory Test routine, position-independent
-;
-mtest0:
-mtest:
-	; A reg contains desired ctl$F2 image
+	ori	ctl$ORG0	; set/keep ORG0 only
 	out	0f2h
-mtest1:		; lands at 03000h - retained relocated code
-	exx
-	mov	h,d
-	mvi	l,0
-	mov	a,b
-	exx
-	mov	c,a
-	mvi	b,2
-mtest2:
-	mov	a,c
-	rlc
-	rlc
-	rlc
-	rlc
-	mov	c,a
-	ani	00fh
-	adi	090h
-	daa
-	aci	040h
-	daa
-	out	0e8h
-mtest3:
-	in	0edh
-	ani	020h
-	jz	mtest3
-	dcr	b
-	jnz	mtest2
+mtest0:
+	lda	seed
+	call	hexout
 	mvi	a,CR
-	out	0e8h
-	exx
-	mov	a,b
+	call	chrout
+mtest1:
+	lda	seed
+	lxi	h,2000h
 mtest4:
 	mov	m,a
 	adi	1
@@ -142,14 +64,9 @@ mtest4:
 	inr	l
 	jnz	mtest4
 	inr	h
-	dcr	c
 	jnz	mtest4
-	mov	a,h
-	sub	d
-	mov	c,a
-	mov	h,d
-	mvi	l,0
-	mov	a,b
+	lda	seed
+	lxi	h,2000h
 mtest5:
 	cmp	m
 	jnz	mtest9
@@ -158,125 +75,45 @@ mtest5:
 	inr	l
 	jnz	mtest5
 	inr	h
-	dcr	c
 	jnz	mtest5
-	exx
-	lxi	h,memtest
-	lxi	d,0
-	lxi	b,mtestZ-mtest1
-	exx
-	mov	a,d
-	xri	030h
-	mov	d,a
-	jz	mtest6
-	mov	c,e
-	jmp	mtest7
-mtest6:
-	mvi	c,030h
-	mvi	a,001h
-	add	b
+
+	lda	seed
+	adi	1
 	daa
-	mov	b,a
-	exx
-	xchg
-	exx
-mtest7:
-	exx
-	call	ldir
-	mov	a,d
-	ani	0f0h
-	mov	h,a
-	mvi	l,0
-	mvi	c,mtestZ-mtest1
-	xra	a
-mtest8:
-	add	m
-	inx	h
-	dcr	c
-	jnz	mtest8
-	mov	c,a
-	exaf
-	cmp	c
-	jnz	mtestE
-	exaf
-	mov	a,d
-	ani	0f0h
-	mov	h,a
-	mvi	l,0
-	pchl
-mtest9:
-	xra	m
-	mov	d,a
-	mvi	a,LF
-	out	0e8h
-mtestA:
-	in	0edh
-	ani	020h
-	jz	mtestA
-	mvi	c,2
-	mvi	b,4
-mtestB:
-	mov	a,h
-	rlc
-	rlc
-	rlc
-	rlc
-	ani	00fh
-	adi	090h
-	daa
-	aci	040h
-	daa
-	out	0e8h
-mtestC:
-	in	0edh
-	ani	020h
-	jz	mtestC
-	dad	h
-	dad	h
-	dad	h
-	dad	h
-	dcr b ! jnz	mtestB
-	mvi	a,' '
-	out	0e8h
-mtestD:
-	in	0edh
-	ani	020h
-	jz	mtestD
-	dcr	c
-	xchg
-	mvi	b,002h
-	jnz	mtestB
+	sta	seed
+	jmp	mtest0
+
+mtest9:	; HL=error addr
+	; print "AAAA DD *" and beep forever
+	call	adrnl
+	call	spout
+	lda	seed
+	call	hexout
+	call	spout
 	mvi	a,'*'
-	out	0e8h
-	jmp	mtestG
-mtestE:
-	in	0edh
-	ani	020h
-	jz	mtestE
-	mvi	a,LF
-	out	0e8h
-mtestF:
-	in	0edh
-	ani	020h
-	jz	mtestF
-	mvi	a,'!'
-	out	0e8h
+	call	chrout
 mtestG:
-	in	0edh
-	ani	020h
-	jz	mtestG
+	mvi	a,BEL
+	call	chrout
 	xra	a
 	mvi	b,0fah
 mtestH:
 	dcr	a
 	jnz	mtestH
-	dcr b ! jnz	mtestH
-	mvi	a,BEL
-	out	0e8h
+	dcr b ! jnz mtestH
 	jmp	mtestG
-; End of relocated code
-mtestZ	equ	$
-endif
+
+chrout:	lhld	conout
+	pchl
+
+waitcr:
+	call	conin
+	cpi	CR
+	jnz	waitcr
+	jmp	crlf
+
+	ds	64
+stack:	ds	0
 
 	rept	(($+0ffh) and 0ff00h)-$
 	db	0ffh
